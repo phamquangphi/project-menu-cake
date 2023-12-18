3afe8df24198778c12bbcdd2d4bb4959a3f72e67
phi phạm <phamquangphibug@gmail.com>
2023-12-18 09:05:10 +0700

    18/12

--- src/components/MenuTemplate.jsx
+++ src/components/MenuTemplate.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 const MenuTemplate = () => {
   return (
     <div>
@@ -8,16 +7,30 @@ const MenuTemplate = () => {
       </h1>
       <div className="grid grid-cols-3 gap-5 mx-[100px] mt-[50px]">
         {/* CROISSANT */}
-        <div className=" bg-white shadow-2xl rounded-3xl w-[300px]">
+        <div className=" bg-white shadow-2xl rounded-3xl">
           <model-viewer
             src="/images/modelview/croissant.glb"
             ar
+            auto-rotate            
             ar-modes="webxr scene-viewer quick-look"
             camera-controls
             poster="poster.webp"
             shadow-intensity="2"
-          ></model-viewer>
-
+            touch-action="pan-y"
+          >
+            <div className="progress-bar hide" slot="progress-bar">
+              <div className="update-bar"></div>
+            </div>
+            <button slot="ar-button" id="ar-button">
+              View in your space
+            </button>
+            <div id="ar-prompt">
+              <img
+                src="https://modelviewer.dev/shared-assets/icons/hand.png"
+                alt=""
+              />
+            </div>
+          </model-viewer>
           <div className="flex items-center justify-between my-[30px] mx-5">
             <h1 className="text-[#9b4427] font-bold text-[20px] underline">
               Croissant
@@ -25,126 +38,28 @@ const MenuTemplate = () => {
             <p className=" text-[#9b4427] font-medium text-[25px]">3.50$</p>
           </div>
         </div>
-        {/* BREAD  */}
-        <div className=" bg-white shadow-2xl rounded-3xl w-[300px]">
-          <model-viewer
-            src="/images/modelview/bread_scan.glb"
-            ar
-            ar-modes="webxr scene-viewer quick-look"
-            camera-controls
-            poster="poster.webp"
-            shadow-intensity="1.34"
-            exposure="0.85"
-            shadow-softness="0.66"
-          ></model-viewer>
-
-          <div className="flex items-center justify-between my-[30px] mx-5">
-            <h1 className="text-[#9b4427] font-bold text-[20px] underline">
-              Bread
-            </h1>
-            <p className=" text-[#9b4427] font-medium text-[25px]">3.00 $</p>
+        <model-viewer
+          src="/images/modelview/cupcake.glb"
+          ar
+          auto-rotate
+          ar-modes="webxr scene-viewer quick-look"
+          camera-controls
+          poster="poster.webp"
+          shadow-intensity="2"
+        >
+          <div className="progress-bar hide" slot="progress-bar">
+            <div className="update-bar"></div>
           </div>
-        </div>
-        {/* PANCAKE */}
-        <div className=" bg-white shadow-2xl rounded-3xl w-[300px]">
-          <model-viewer
-            src="/images/modelview/pancake.glb"
-            ar
-            ar-modes="webxr scene-viewer quick-look"
-            camera-controls
-            poster="poster.webp"
-            shadow-intensity="1.58"
-            exposure="0.85"
-            shadow-softness="0.87"
-          ></model-viewer>
-
-          <div className="flex items-center justify-between my-[30px] mx-5">
-            <h1 className="text-[#9b4427] font-bold text-[20px] underline">
-              Pancake
-            </h1>
-            <p className=" text-[#9b4427] font-medium text-[25px]">7.50 $</p>
+          <button slot="ar-button" id="ar-button">
+            View in your space
+          </button>
+          <div id="ar-prompt">
+            <img
+              src="https://modelviewer.dev/shared-assets/icons/hand.png"
+              alt=""
+            />
           </div>
-        </div>
-        {/* taco */}
-        <div className=" bg-white shadow-2xl rounded-3xl w-[300px]">
-          <model-viewer
-            src="/images/modelview/taco.glb"
-            ar
-            ar-modes="webxr scene-viewer quick-look"
-            camera-controls
-            poster="poster.webp"
-            shadow-intensity="1.65"
-            exposure="0.85"
-            shadow-softness="1"
-          ></model-viewer>
-
-          <div className="flex items-center justify-between my-[30px] mx-5">
-            <h1 className="text-[#9b4427] font-bold text-[20px] underline">
-              Tacos
-            </h1>
-            <p className=" text-[#9b4427] font-medium text-[25px]">6.50 $</p>
-          </div>
-        </div>
-        {/* WAFFLE */}
-        <div className=" bg-white shadow-2xl rounded-3xl w-[300px]">
-          <model-viewer
-            src="/images/modelview/waffles.glb"
-            ar
-            ar-modes="webxr scene-viewer quick-look"
-            camera-controls
-            poster="poster.webp"
-            shadow-intensity="0.93"
-            exposure="0.66"
-            shadow-softness="0.39"
-          ></model-viewer>
-
-          <div className="flex items-center justify-between my-[30px] mx-5">
-            <h1 className="text-[#9b4427] font-bold text-[20px] underline">
-              Waffle
-            </h1>
-            <p className=" text-[#9b4427] font-medium text-[25px]">6.00 $</p>
-          </div>
-        </div>
-        {/* CUPCAKE */}
-        <div className=" bg-white shadow-2xl rounded-3xl w-[300px]">
-          <model-viewer
-            src="/images/modelview/cupcake.glb"
-            ar
-            ar-modes="webxr scene-viewer quick-look"
-            camera-controls
-            poster="poster.webp"
-            shadow-intensity="0.93"
-            exposure="0.66"
-            shadow-softness="0.39"
-          ></model-viewer>
-
-          <div className="flex items-center justify-between my-[30px] mx-5">
-            <h1 className="text-[#9b4427] font-bold text-[20px] underline">
-              Cupcake
-            </h1>
-            <p className=" text-[#9b4427] font-medium text-[25px]">5.50 $</p>
-          </div>
-        </div>
-        {/* DOUGHNUT */}
-        <div className=" bg-white shadow-2xl rounded-3xl w-[300px]">
-          <model-viewer
-            src="/images/modelview/doughnut.glb"
-            ar
-            ar-modes="webxr scene-viewer quick-look"
-            camera-controls
-            poster="poster.webp"
-            shadow-intensity="0.93"
-            exposure="0.66"
-            shadow-softness="0.39"
-          ></model-viewer>
-
-          <div className="flex items-center justify-between my-[30px] mx-5">
-            <h1 className="text-[#9b4427] font-bold text-[20px] underline">
-              Doughnut
-            </h1>
-            <p className=" text-[#9b4427] font-medium text-[25px]">5.00 $</p>
-          </div>
-        </div>
+        </model-viewer>
       </div>
     </div>
   );
